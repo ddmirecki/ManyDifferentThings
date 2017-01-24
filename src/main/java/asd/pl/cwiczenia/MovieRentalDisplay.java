5565551cfddf4a8969ca20f28d876ff195995a64
Daniel <ddmirecki@gmail.com>
2017-01-24 16:06:54 +0100

    cwiczenia zadanie domowe - bledy przy odczycie z pliku

--- src/main/java/asd/pl/cwiczenia/MovieRentalDisplay.java
+++ src/main/java/asd/pl/cwiczenia/MovieRentalDisplay.java
@@ -4,7 +4,11 @@ import asd.pl.cwiczenia.exceptions.CustomerAlreadyExistsException;
 import asd.pl.cwiczenia.exceptions.MovieAlreadyExistsException;
 import asd.pl.cwiczenia.exceptions.MyException;
 import asd.pl.cwiczenia.exceptions.RentalNotFoundException;
+import com.google.gson.Gson;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -13,6 +17,7 @@ import java.util.Scanner;
 public class
 MovieRentalDisplay {
     MovieRental movieRental;
+    Gson gson = new Gson();
 
 
     public MovieRentalDisplay() throws MyException, CustomerAlreadyExistsException, MovieAlreadyExistsException {
@@ -30,7 +35,7 @@ MovieRentalDisplay {
     }
 
 
-    public void run() throws RentalException, MovieAlreadyExistsException, RentalNotFoundException, MyException {
+    public void run() throws RentalException, MovieAlreadyExistsException, RentalNotFoundException, MyException, IOException {
         displayTitle();
 
         while (true) {
@@ -50,44 +55,59 @@ MovieRentalDisplay {
             int choosedOption = new Scanner(System.in).nextInt();
 
 
+            if (choosedOption != 0) {
 
-            switch (choosedOption){
-                case 1 :
-                    System.out.println(movieRental.getCustomers());
-                    break;
-                case 2 :
-                    System.out.println(movieRental.getMovies() );
-                    break;
-                case 3 :
-                    System.out.println(movieRental.getRentals());
-                    break;
-                case 4:
-                    System.out.println("Podaj ID klienta");
-                    String customerID = new Scanner(System.in).nextLine();
-                    System.out.println("Podaj ID filmu");
-                    String movieID = new Scanner(System.in).nextLine();
-                    movieRental.rent(customerID, movieID);
-                    break;
-                case 5 :
-                    System.out.println("Podaj ID wypozyczenia");
-                    String rentalID = new Scanner(System.in).nextLine();
-                    movieRental.returnMovie(rentalID);
-                    break;
-                case 6 :
-                    System.out.println("podaj dane filmu");
-                    String movieText = new Scanner(System.in).nextLine();
-                    Movie movie = new Movie(movieText);
-                    movieRental.addMovie(movie = new Movie(movieText));
 
+                switch (choosedOption) {
+                    case 1:
+                        System.out.println(movieRental.getCustomers());
+                        break;
+                    case 2:
+                        System.out.println(movieRental.getMovies());
+                        break;
+                    case 3:
+                        System.out.println(movieRental.getRentals());
+                        break;
+                    case 4:
+                        System.out.println("Podaj ID klienta");
+                        String customerID = new Scanner(System.in).nextLine();
+                        System.out.println("Podaj ID filmu");
+                        String movieID = new Scanner(System.in).nextLine();
+                        movieRental.rent(customerID, movieID);
+                        break;
+                    case 5:
+                        System.out.println("Podaj ID wypozyczenia");
+                        String rentalID = new Scanner(System.in).nextLine();
+                        movieRental.returnMovie(rentalID);
+                        break;
+                    case 6:
+                        System.out.println("podaj dane filmu");
+                        String movieText = new Scanner(System.in).nextLine();
+                        Movie movie = new Movie(movieText);
+                        movieRental.addMovie(movie = new Movie(movieText));
+                    case 7:
+                        String json = gson.toJson(movieRental.getCustomers());
+                        FileWriter fileWriter = new FileWriter("rental.json");
+                        gson.toJson(json, fileWriter);
+                        fileWriter.flush();
+                        fileWriter.close();
+                        System.out.println(json);
+                        break;
+                    case 8:
+                      MovieRental movieRental = gson.fromJson(new FileReader("rental.json"), MovieRental.class);
+                        System.out.println(movieRental);
+
+
+                }
+
+            } else {
+                break;
 
 
             }
 
-
         }
-
     }
-
 
     private void displayTitle() {
         for (int i = 0; i < 30; i++) {
@@ -102,7 +122,7 @@ MovieRentalDisplay {
         }
     }
 
-    public static void main(String[] args) throws CustomerAlreadyExistsException, MovieAlreadyExistsException, MyException, RentalNotFoundException, RentalException {
+    public static void main(String[] args) throws CustomerAlreadyExistsException, MovieAlreadyExistsException, MyException, RentalNotFoundException, RentalException, IOException {
         new MovieRentalDisplay().run();
 
     }
